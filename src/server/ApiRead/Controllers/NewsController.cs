5641ae8374620472511038826dd865c227265dd3
m.Sarafraz <m.Sarafraz@raisasys.com>
2024-08-01 12:07:53 +0330

    Change

--- src/server/ApiRead/Controllers/NewsController.cs
+++ src/server/ApiRead/Controllers/NewsController.cs
@@ -4,17 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 using Org.BouncyCastle.Utilities.Encoders;
 using Queries;
 using System.Net;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace ApiRead
 {
 	public class NewsController : AppController
 	{
 		[HttpGet]
-		public async Task<ActionResult<NewsFullDto>> GetById([FromQuery] GetNewsById query)
-		{
-			try
+		public async Task<ActionResult<NewsFullDto>> GetById([FromQuery] long newsId)
+        {
+            try
 			{
-                var oResponse = await QueryProcessor.Execute<GetNewsById, NewsFullDto>(query);
+                var oResponse = await QueryProcessor.Execute<GetNewsById, NewsFullDto>(new GetNewsById { NewsId = newsId });
                 return Ok(oResponse);
             }
             catch (Exception oEx)
@@ -41,7 +42,8 @@ namespace ApiRead
 		{
 			try
 			{
-				return Ok(await QueryProcessor.Execute<GetNewsListDto, NewsListDto>(oQueryString));
+				var oResponse = await QueryProcessor.Execute<GetNewsListDto, NewsListDto>(oQueryString);
+                return Ok(oResponse);
 			}
 			catch (Exception oEx)
 			{
